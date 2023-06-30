cd7e52560c1dc79d372055698e6e5082f2907e00
xelser <dkzenzuri@gmail.com>
2023-06-30 23:52:42 +0800

    new upload

--- upload.sh
+++ upload.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp -rf $HOME/.local/share/backgrounds/* 		$HOME/Documents/gnome-backgrounds-day-night/
-cp -rf $HOME/.local/share/gnome-background-properties/* $HOME/Documents/gnome-backgrounds-day-night/
+cd $HOME/Documents/gnome-backgrounds-day-night && rm -rf backgrounds gnome-background-properties
+
+cp -rf $HOME/.local/share/backgrounds 			$HOME/Documents/gnome-backgrounds-day-night/
+cp -rf $HOME/.local/share/gnome-background-properties	$HOME/Documents/gnome-backgrounds-day-night/
 
 cd $HOME/Documents/gnome-backgrounds-day-night
-git fetch && git pull && git add . && git commit -m 'auto-update @ $(hostname)' && git push
+git fetch && git pull && git add . && git commit -m 'new upload' && git push
